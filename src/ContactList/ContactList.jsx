60b6beb8ba4d4b2ec9e677511a85184f3a450358
Dumenco <tt-3484@ukr.net>
2020-06-09 09:01:04 +0300

    homePhone2

---
+++ src/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import T from 'prop-types';
+import React from 'react';
+
+const ContactList = ({ contacts, onDelete }) => (
+  <ul>
+    {contacts.map(contact => (
+      <li key={contact.id}>
+        {contact.name}:{contact.number}
+        <button type="button" onClick={() => onDelete(contact.id)}>
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+ContactList.propTypes = {
+  contacts: T.arrayOf(
+    T.shape({
+      id: T.string.isRequired,
+      name: T.string.isRequired,
+      number: T.string.isRequired,
+    }),
+  ).isRequired,
+  onDelete: T.func.isRequired,
+};
+
+export default ContactList;
