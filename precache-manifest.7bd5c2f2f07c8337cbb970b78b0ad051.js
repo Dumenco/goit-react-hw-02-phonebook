cccc006e891e5d741dcbcdce858c632393e7e2b4
Dumenco <tt-3484@ukr.net>
2020-06-09 09:08:46 +0300

    Updates

---
+++ precache-manifest.7bd5c2f2f07c8337cbb970b78b0ad051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230277ae0204449ed72c157d653d01d1",
+    "url": "//dumenco.github.io/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "7148ac776ba9f63fda48",
+    "url": "//dumenco.github.io/goit-react-hw-02-phonebook/static/css/main.74613879.chunk.css"
+  },
+  {
+    "revision": "07368543704a51b95de7",
+    "url": "//dumenco.github.io/goit-react-hw-02-phonebook/static/js/2.0e0d17f3.chunk.js"
+  },
+  {
+    "revision": "7148ac776ba9f63fda48",
+    "url": "//dumenco.github.io/goit-react-hw-02-phonebook/static/js/main.f9472b6a.chunk.js"
+  },
+  {
+    "revision": "d9b601995ede4952e9ef",
+    "url": "//dumenco.github.io/goit-react-hw-02-phonebook/static/js/runtime-main.219414b5.js"
+  }
+]);
